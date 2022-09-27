effb02ff816bffa0a83527336aae6ae9ff22560c
praveen3r <45786671+praveen3r@users.noreply.github.com>
2022-09-27 14:08:24 +0530

    Update UserSearch.tsx

--- Sample-Projects/MERN stack/front-end/src/components/home/UserSearch.tsx
+++ Sample-Projects/MERN stack/front-end/src/components/home/UserSearch.tsx
@@ -1,8 +1,63 @@
+import { Form, Formik, FormikHelpers } from 'formik';
 import React from 'react'
+import { UserSearchType } from '../../types/FormTypes';
+import InputComponent from '../formik-control/InputComponent';
+
+const initialValues: UserSearchType = {
+  name: "",
+  fromDate: "",
+  toDate: ""
+};
 
 function UserSearch() {
+
+  const onSubmit = (
+    values: UserSearchType,
+    submitProps: FormikHelpers<UserSearchType>
+  ) => {
+    console.log(`coming here`);
+
+  };
+
   return (
-    <div>UserSearch</div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="form-group">
+          <InputComponent
+            label="name"
+            type="text"
+            id="name"
+            name="name"
+            maxLength="15"
+            dataType="text"
+            width="20%"
+          />
+
+          <InputComponent
+            label="fromDate"
+            type="text"
+            id="fromDate"
+            name="fromDate"
+            maxLength="15"
+            dataType="text"
+            width="20%"
+          />
+
+          <InputComponent
+            label="toDate"
+            type="text"
+            id="toDate"
+            name="toDate"
+            maxLength="15"
+            dataType="text"
+            width="20%"
+          />
+        </div>
+      </Form>
+    </Formik>
   )
 }
 
