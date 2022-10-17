e73ea9db2f9d44a4bfab0b369df5a57c9f4f597f
praveen3r <45786671+praveen3r@users.noreply.github.com>
2022-10-17 18:13:16 +0530

    Update job-info.service.ts

--- Sample-Projects/Angular - SpringBoot/front-end/src/app/job-info/job-info.service.ts
+++ Sample-Projects/Angular - SpringBoot/front-end/src/app/job-info/job-info.service.ts
@@ -1,1 +1,17 @@
 
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class JobInfoService {
+
+    constructor(private http: HttpClient) { }
+
+    getCourses(data?: string): Observable<any> {
+        return this.http.get<any>('http://localhost:8080/v1/users/');
+      }
+
+  }
